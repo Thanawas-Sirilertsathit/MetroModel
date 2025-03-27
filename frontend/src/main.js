fa6bea3d3b4e4096a8107b47f451fc91109259dd
Thanawas Sirilertsathit <146911997+Thanawas-Sirilertsathit@users.noreply.github.com>
2025-03-27 09:17:59 +0700

    change location of styles.css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import './assets/styles.css';
+import './styles/styles.css';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(router).mount('#app');
