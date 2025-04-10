4bfdd9e5eaa18a16d5586fd4639063c3dcd8913b
Thanawas Sirilertsathit <146911997+Thanawas-Sirilertsathit@users.noreply.github.com>
2025-04-10 13:48:30 +0700

    format the srt_test_prediction file

--- data_analytics/constant.py
+++ data_analytics/constant.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 RATING_MAP = {"Very Low": 0, "Low": 1, "Moderate": 2, "Crowded": 3, "Dense": 4, "Very Dense": 5}
 EXAMPLE_DATA = pd.DataFrame({
-    "Hour": [9, 13, 17],    
-    "Time_Block": ["Morning", "Afternoon", "Evening"],
+    "Hour": [19, 13, 17],    
+    "Time_Block": ["Evening", "Afternoon", "Evening"],
     "Day_of_Week": ["Saturday", "Friday", "Sunday"],
-    "temperature_c": [30.2, 36.5, 37.5],
+    "temperature_c": [29.4, 36.5, 37.5],
     "humidity": [70, 82, 50],
-    "pressure_mb": [1005.1, 1003.7, 1100.2],
+    "pressure_mb": [1008, 1003.7, 1100.2],
 })
--- data_analytics/srt_test_prediction.py
+++ data_analytics/srt_test_prediction.py
@@ -6,15 +6,19 @@ nb = SRTNaiveBayes()
 df = nb.preprocess()
 nb.train(df)
 nb.evaluate()
+
 qr = SRTQuantileRegressor(quantile=0.48)
 X_train, X_test, y_train, y_test = qr.preprocess(df, RATING_MAP)
 qr.train(X_train, y_train, X_test, y_test)
 qr.evaluate()
+
 example_data = EXAMPLE_DATA
 predicted_ratings = nb.predict(example_data)
 print("Naive Bayes Predicted Ratings:", predicted_ratings.tolist())
+
 example_data = qr.rating_map(example_data, predicted_ratings, RATING_MAP)
 print(example_data)
+
 predicted_counts = qr.predict(example_data)
 predicted_counts = [int(round(x)) for x in predicted_counts]
 print("Quantile Regression Predicted Passenger Count:", predicted_counts)
