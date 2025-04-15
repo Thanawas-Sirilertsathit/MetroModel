7a5dc46f3326010305b9084d11d6332eb1afd5ca
Sorasit Kateratorn <sorasit.ka@ku.th>
2025-04-15 16:33:09 +0700

    merges hourly March weather data from CSV with existing data in the database sorts it

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import SampleDataView, ProjectHourlyAverageAPIView, ProjectOnedayAverageAPIView
-
+from .views_weather import MergedWeatherAPIView
 urlpatterns = [
     path('sample/', SampleDataView.as_view(), name='sample-data'),
     path('hourly/', ProjectHourlyAverageAPIView.as_view(), name='hourly'),
     path('oneday/', ProjectOnedayAverageAPIView.as_view(), name='oneday'),
+        path('hourly-merged/', MergedWeatherAPIView.as_view(), name='hourly-merged'),
 ]
