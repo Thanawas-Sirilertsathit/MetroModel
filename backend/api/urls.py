d176f6e314097b3d6619af54fa6ef0ef0655109d
Sorasit Kateratorn <sorasit.ka@ku.th>
2025-04-15 19:33:27 +0700

    reset create new api endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import SampleDataView, ProjectHourlyAverageAPIView, ProjectOnedayAverageAPIView
-from .views_weather import MergedWeatherAPIView
 urlpatterns = [
     path('sample/', SampleDataView.as_view(), name='sample-data'),
     path('hourly/', ProjectHourlyAverageAPIView.as_view(), name='hourly'),
     path('oneday/', ProjectOnedayAverageAPIView.as_view(), name='oneday'),
-        path('hourly-merged/', MergedWeatherAPIView.as_view(), name='hourly-merged'),
 ]
